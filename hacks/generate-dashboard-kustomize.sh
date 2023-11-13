9ea2810fe9a1da3c72980fbf8aa8e516b2c2098f
Sakura Dragon <37706595+sakura-dragon@users.noreply.github.com>
2023-11-13 20:56:48 +0000

    Update dashboard annotations

--- hacks/generate-dashboard-kustomize.sh
+++ hacks/generate-dashboard-kustomize.sh
@@ -10,12 +10,16 @@ $(find generated -iname '*.json' | while read line; do echo "  - $line"; done)
     labels:
       grafana_dashboard: "1"
       app.kubernetes.io/component: monitoring
-- name: dashboards-static
+    annotations:
+      grafana_folder: "kubernetes"
+- name: dashboards-flux
   files:
-$(find static -iname '*.json' | while read line; do echo "  - $line"; done)
+$(find static -iname 'flux*.json' | while read line; do echo "  - $line"; done)
   options:
     labels:
       grafana_dashboard: "1"
       app.kubernetes.io/component: monitoring
+    annotations:
+      grafana_folder: "flux"
 EOF
 cd -
